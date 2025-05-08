7d4cd03df47fdec9cc45f7bf66735020bb2b189c
baianjo <1987892914@qq.com>
2025-05-08 22:13:19 +0800

    更多叹号

--- HelloWorldProject/src/Main.java
+++ HelloWorldProject/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World from workspace1 - final updated!");
-        System.out.println("modified!!!");
+        System.out.println("modified!!!!!!!!!!!!!!!!!!!!");
     }
 }
